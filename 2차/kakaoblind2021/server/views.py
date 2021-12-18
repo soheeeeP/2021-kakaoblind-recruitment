bc81aa2f8fc892554ae1d56b11a7b7715e27854c
soheeeeP <tpfj@naver.com>
2021-12-18 18:36:00 +0900

    #5 REFACTOR: scheduler를 프로젝트 변수로 사용

--- 2차/kakaoblind2021/server/views.py
+++ 2차/kakaoblind2021/server/views.py
@@ -5,8 +5,9 @@ from django.db import transaction
 from rest_framework import generics, status
 from rest_framework.response import Response
 
-from kakaoT.models import Problem, SERVER_STATUS
-from utils.scheduler import KakaoTScheduler
+from kakaoT.models import Problem
+from kakaoblind2021 import settings
+from utils.scheduler import KakaoTScheduler, SERVER_STATUS
 
 
 class SimulateView(generics.CreateAPIView):
@@ -24,15 +25,36 @@ class SimulateView(generics.CreateAPIView):
             message = "INVALID AUTH_KEY ERROR"
             return Response({"message": message}, status=status.HTTP_400_BAD_REQUEST)
 
+        # 테스트용 데이터 (problem 0)
+        # test_data = {
+        #   "0": [[3, 0, 10]],
+        #   "1": [[1, 3, 1], [1, 4, 15]],
+        #   "2": [[0, 3, 2], [3, 1, 4], [0, 3, 1]],
+        #   "3": [[1, 3, 5]]
+        # }
+
+        # 서버(시나리오)의 scheduler에 작업을 추가
+        kakao = getattr(settings, 'kakao')
+        print(kakao.server_status)
+        if kakao.server_status == SERVER_STATUS.initial:
+            kakao.init_scheduler(scenario=p.idx)
+        elif kakao.server_status == SERVER_STATUS.finished:
+            kakao.termiate_scheduler()
+            delattr(settings, 'kakao')
+            kakao = KakaoTScheduler()
+            setattr(settings, 'kakao', kakao)
+            kakao.init_scheduler(scenario=p.idx)
+
         req_url = self.base_url + str(idx) + "_day-1.json"
         rental_req_data = requests.get(req_url).json()
 
-        commands = json.loads(request.POST['commands'])
+        # 자전거 대여 요청 수행
+        kakao.add_bike_scheduler(problem=p, rental=rental_req_data)
+        kakao.set_server_scenario_score()
+
+        commands = request.POST.get('commands', None)
         if commands:    # 트럭 이동 요청이 들어온 경우
-            kakao = KakaoTScheduler(problem=p, rental=rental_req_data, truck_mode=True, commands=commands)
-        else:           # 트럭 이동 없이 자전거 대여 요청만 수행하는 경우
-            kakao = KakaoTScheduler(problem=p, rental=rental_req_data, truck_mode=False, commands=None)
-        transaction.on_commit(lambda: kakao.init_scheduler())
+            kakao.add_truck_scheduler(commands=json.loads(commands))
 
         response = {
             "status": kakao.server_status,
