fd288491916abb037fae7acf719cf4fd823428ac
soheeeeP <tpfj@naver.com>
2021-12-18 17:18:58 +0900

    #1 ADD: 모델 생성(Problem, Location, Truck)

--- 2차/kakaoblind2021/kakaoT/models.py
+++ 2차/kakaoblind2021/kakaoT/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Problem(models.Model):
+    idx = models.PositiveSmallIntegerField()
+    auth_key = models.UUIDField()
+
+
+class Location(models.Model):
+    problem = models.ForeignKey(
+        Problem,
+        on_delete=models.CASCADE,
+        verbose_name='problem'
+    )
+    idx = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0)]
+    )
+    row = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0)],
+        default=0
+    )
+    col = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0)],
+        default=0
+    )
+    bike = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0)],
+        default=0
+    )
+
+
+class Truck(models.Model):
+    problem = models.ForeignKey(
+        Problem,
+        on_delete=models.CASCADE,
+        verbose_name='problem'
+    )
+    idx = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0)],
+        default=0
+    )
+    loc_row = models.PositiveSmallIntegerField(default=0)
+    loc_col = models.PositiveSmallIntegerField(default=0)
+    loc_idx = models.PositiveSmallIntegerField(default=0)
+    bikes = models.PositiveSmallIntegerField(default=0)
